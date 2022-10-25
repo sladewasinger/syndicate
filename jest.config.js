08bfcc1ecef9cfb607db87c6ea61331c148651e3
Austin Wasinger <austinwasinger@gmail.com>
2022-10-24 19:10:38 -0600

    Fixed text

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,7 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
-export default {
+module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  testMatch: ['**/*.test.ts'],
+  testPathIgnorePatterns: ['/node_modules/', '/dist/', '/app/'],
 };
