c307d029c09457bcf112ae57205c138a4840f344
Austin Wasinger <austinwasinger@gmail.com>
2022-12-02 10:04:55 -0700

    Fixed mortgaged charging players

--- server/src/models/tiles/SubwayTile.ts
+++ server/src/models/tiles/SubwayTile.ts
@@ -31,7 +31,7 @@ export class SubwayTile implements IBuyableTile {
 
   onLanded(gameData: GameData, currentState: StateName): StateName {
     if (this.owner != null) {
-      if (this.owner !== gameData.currentPlayer) {
+      if (this.owner !== gameData.currentPlayer && !this.mortgaged) {
         gameData.currentPlayer.money -= this.entranceFee(gameData);
         this.owner.money += this.entranceFee(gameData);
       }
